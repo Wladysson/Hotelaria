ec4c125d10de0d35f2309f07097b1237edeb5e89
wladysson <wladysonaraujo991@gmail.com>
2026-08-16 17:10:00 -0300

    feat(notification): implementar dependências da camada de API, disponibiliza sessão através do sistema de dependências do FastAPI, mantém a implementação de infraestrutura desacoplada dos endpoints

---
+++ apps/services/notification-service/src/api/deps.py
@@ -0,0 +1,11 @@
+from collections.abc import AsyncGenerator
+
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from src.core import get_db_session
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async for session in get_db_session():
+        yield session
